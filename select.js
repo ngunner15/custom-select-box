d573eb6008f83a72db49f524e1b3795bb438562a
Nisarg Vadgama <nisargvv@gmail.com>
2021-03-03 19:28:57 -0500

    create options as li tags and append them to ul

--- select.js
+++ select.js
@@ -8,21 +8,34 @@ export default class Select {
     setupCustomElement(this)
     element.after(this.customElement)
   }
+
+  get selectedOption() {
+    return this.options.find(option => option.selected)
+  }
 }
 
 function setupCustomElement(select) {
   select.customElement.classList.add('custom-select-container')
 
   select.labelElement.classList.add('custom-select-value')
+  select.labelElement.innerText = select.selectedOption.label
   select.customElement.append(select.labelElement)
 
   select.optionsCustomElement.classList.add('custom-select-options')
+  select.options.forEach(option => {  // create options as li tags and append them to ul
+    const optionElement = document.createElement('li')
+    optionElement.classList.add('custom-select-option')
+    optionElement.classList.toggle('selected', option.selected)
+    optionElement.innerText = option.label
+    optionElement.dataset.value = option.value
+    select.optionsCustomElement.append(optionElement)
+  });
   select.customElement.append(select.optionsCustomElement)
 }
 
 function getFormattedOptions(optionElements) {
-  [...optionElements].map(optionElement => {
-    return {
+  return [...optionElements].map(optionElement => {  // Convert multiple option elements under select tag to an array
+    return {                                         // return multiple option elements under select tag in an object
       value: optionElement.value,
       label: optionElement.label,
       selected: optionElement.selected,
